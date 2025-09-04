faba117e6582dc9a41b98dd4117750adacde5587
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 23:10:38 +0100

    Updated AuthWrapper1.tsx

--- src/views/pages/authentication/AuthWrapper1.tsx
+++ src/views/pages/authentication/AuthWrapper1.tsx
@@ -1,24 +1,21 @@
-@@ .. @@
-+import type { FC, ReactNode } from 'react';
-+
- // material-ui
- import { styled } from '@mui/material/styles';
+import type { FC, ReactNode } from 'react';
 
-+interface AuthWrapper1Props {
-+  children: ReactNode;
-+}
-+
- // ==============================|| AUTHENTICATION 1 WRAPPER ||============================== //
+// material-ui
+import { styled } from '@mui/material/styles';
 
--const AuthWrapper1 = styled('div')(({ theme }) => ({
-+const AuthWrapper1Styled = styled('div')(({ theme }) => ({
-   backgroundColor: theme.palette.grey[100],
-   minHeight: '100vh'
- }));
+interface AuthWrapper1Props {
+  children: ReactNode;
+}
 
--export default AuthWrapper1;
-+const AuthWrapper1: FC<AuthWrapper1Props> = ({ children }) => {
-+  return <AuthWrapper1Styled>{children}</AuthWrapper1Styled>;
-+};
-+
-+export default AuthWrapper1;
+// ==============================|| AUTHENTICATION 1 WRAPPER ||============================== //
+
+const AuthWrapper1Styled = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.grey[100],
+  minHeight: '100vh'
+}));
+
+const AuthWrapper1: FC<AuthWrapper1Props> = ({ children }) => {
+  return <AuthWrapper1Styled>{children}</AuthWrapper1Styled>;
+};
+
+export default AuthWrapper1;
