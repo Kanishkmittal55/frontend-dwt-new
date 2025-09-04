05f595454453be01bf9497f5eb58ccaa1b64396f
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 22:57:49 +0100

    Updated index.tsx

--- src/layout/MainLayout/MenuList/NavCollapse/index.tsx
+++ src/layout/MainLayout/MenuList/NavCollapse/index.tsx
@@ -1,33 +1,154 @@
-@@ .. @@
- import PropTypes from 'prop-types';
- import { useEffect, useRef, useState } from 'react';
- import { matchPath, useLocation } from 'react-router-dom';
-+import type { FC } from 'react';
-+import type { MenuItem } from 'types';
+import PropTypes from 'prop-types';
+import { useEffect, useRef, useState } from 'react';
+import { matchPath, useLocation } from 'react-router-dom';
+import type { FC } from 'react';
+import type { MenuItem } from 'types';
 
- // material-ui
- import { useTheme } from '@mui/material/styles';
-@@ .. @@
- import { IconChevronDown, IconChevronRight, IconChevronUp } from '@tabler/icons-react';
- import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Collapse, List, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
 
--export default function NavCollapse({ menu, level, parentId }) {
-+interface NavCollapseProps {
-+  menu: MenuItem;
-+  level: number;
-+  parentId?: string;
-+}
-+
-+const NavCollapse: FC<NavCollapseProps> = ({ menu, level, parentId }) => {
-   const theme = useTheme();
-   const ref = useRef(null);
+// project imports
+import NavItem from '../NavItem';
 
-@@ .. @@
-       )}
-     </>
-   );
--}
-+};
+// assets
+import { IconChevronDown, IconChevronRight, IconChevronUp } from '@tabler/icons-react';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
--NavCollapse.propTypes = { menu: PropTypes.any, level: PropTypes.number, parentId: PropTypes.string };
-+export default NavCollapse;
+interface NavCollapseProps {
+  menu: MenuItem;
+  level: number;
+  parentId?: string;
+}
+
+const NavCollapse: FC<NavCollapseProps> = ({ menu, level, parentId }) => {
+  const theme = useTheme();
+  const ref = useRef(null);
+
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const { pathname } = useLocation();
+
+  const handleClick = () => {
+    setOpen(!open);
+    setSelected(!selected ? menu.id : null);
+  };
+
+  // menu collapse & item
+  const menus = menu.children?.map((item) => {
+    switch (item.type) {
+      case 'collapse':
+        return <NavCollapse key={item.id} menu={item} level={level + 1} parentId={parentId} />;
+      case 'item':
+        return <NavItem key={item.id} item={item} level={level + 1} />;
+      default:
+        return (
+          <Typography key={item.id} variant="h6" color="error" align="center">
+            Fix - Collapse or Item
+          </Typography>
+        );
+    }
+  });
+
+  const Icon = menu.icon;
+  const menuIcon = menu.icon ? (
+    <Icon stroke={1.5} size="1.3rem" style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+  ) : (
+    <FiberManualRecordIcon
+      sx={{
+        width: selected === menu.id ? 8 : 6,
+        height: selected === menu.id ? 8 : 6
+      }}
+      fontSize={level > 0 ? 'inherit' : 'medium'}
+    />
+  );
+
+  let menuIconClass = !menu.icon ? { marginTop: 'auto', marginBottom: 'auto' } : {};
+
+  useEffect(() => {
+    const isFound = menu.children?.some((item) => {
+      if (item?.url) {
+        return !!matchPath(
+          {
+            path: item.url,
+            exact: false
+          },
+          pathname
+        );
+      }
+      return false;
+    });
+    if (isFound) {
+      setOpen(true);
+      setSelected(menu.id);
+    }
+  }, [pathname, menu.children, menu.id]);
+
+  // menu collapse for sub-menu list
+  return (
+    <>
+      <ListItemButton
+        ref={ref}
+        sx={{
+          borderRadius: `${theme.shape.borderRadius}px`,
+          mb: 0.5,
+          alignItems: 'flex-start',
+          backgroundColor: level > 1 ? 'transparent !important' : 'inherit',
+          py: level > 1 ? 1 : 1.25,
+          pl: `${level * 24}px`
+        }}
+        selected={selected === menu.id}
+        onClick={handleClick}
+      >
+        <ListItemIcon sx={{ my: 'auto', minWidth: !menu.icon ? 18 : 36, ...menuIconClass }}>
+          {menuIcon}
+        </ListItemIcon>
+        <ListItemText
+          primary={
+            <Typography variant={selected === menu.id ? 'h5' : 'body1'} color="inherit">
+              {menu.title}
+            </Typography>
+          }
+          secondary={
+            menu.caption && (
+              <Typography variant="caption" sx={{ ...theme.typography.subMenuCaption }} display="block" gutterBottom>
+                {menu.caption}
+              </Typography>
+            )
+          }
+        />
+        {open ? (
+          <IconChevronUp stroke={1.5} size="1rem" style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+        ) : (
+          <IconChevronDown stroke={1.5} size="1rem" style={{ marginTop: 'auto', marginBottom: 'auto' }} />
+        )}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        {open && (
+          <List
+            component="div"
+            disablePadding
+            sx={{
+              position: 'relative',
+              '&:after': {
+                content: "''",
+                position: 'absolute',
+                left: '32px',
+                top: 0,
+                height: '100%',
+                width: '1px',
+                opacity: 1,
+                background: theme.palette.primary.light
+              }
+            }}
+          >
+            {menus}
+          </List>
+        )}
+      </Collapse>
+    </>
+  );
+};
+
+export default NavCollapse;
