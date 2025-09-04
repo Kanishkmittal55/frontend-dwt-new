feec5a91d4f9f944282908e362e6201aaf55aec2
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 22:43:28 +0100

    Updated FormControl.tsx

--- src/ui-component/extended/Form/FormControl.tsx
+++ src/ui-component/extended/Form/FormControl.tsx
@@ -1,38 +1,74 @@
-@@ .. @@
--import PropTypes from 'prop-types';
- // material-ui
- import Divider from '@mui/material/Divider';
- import InputAdornment from '@mui/material/InputAdornment';
- import InputLabel from '@mui/material/InputLabel';
- import OutlinedInput from '@mui/material/OutlinedInput';
- import MUIFormControl from '@mui/material/FormControl';
-+import type { FormControlProps } from 'types/utilities';
- 
--export default function FormControl({ captionLabel, formState, iconPrimary, iconSecondary, placeholder, textPrimary, textSecondary }) {
-+export default function FormControl({ 
-+  captionLabel, 
-+  formState, 
-+  iconPrimary, 
-+  iconSecondary, 
-+  placeholder, 
-+  textPrimary, 
-+  textSecondary 
-+}: FormControlProps) {
-   const IconPrimary = iconPrimary;
-   const primaryIcon = iconPrimary ? <IconPrimary fontSize="small" sx={{ color: 'grey.700' }} /> : null;
- 
-@@ -58,14 +65,4 @@ export default function FormControl({ captionLabel, formState, iconPrimary, ico
-       />
-     </MUIFormControl>
-   );
- }
--
--FormControl.propTypes = {
--  captionLabel: PropTypes.string,
--  formState: PropTypes.string,
--  iconPrimary: PropTypes.any,
--  iconSecondary: PropTypes.any,
--  placeholder: PropTypes.string,
--  textPrimary: PropTypes.string,
--  textSecondary: PropTypes.string
--};
+import PropTypes from 'prop-types';
+// material-ui
+import Divider from '@mui/material/Divider';
+import InputAdornment from '@mui/material/InputAdornment';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import MUIFormControl from '@mui/material/FormControl';
+import type { FormControlProps } from 'types/utilities';
+
+export default function FormControl({ 
+  captionLabel, 
+  formState, 
+  iconPrimary, 
+  iconSecondary, 
+  placeholder, 
+  textPrimary, 
+  textSecondary 
+}: FormControlProps) {
+  const IconPrimary = iconPrimary;
+  const primaryIcon = iconPrimary ? <IconPrimary fontSize="small" sx={{ color: 'grey.700' }} /> : null;
+
+  const IconSecondary = iconSecondary;
+  const secondaryIcon = iconSecondary ? <IconSecondary fontSize="small" sx={{ color: 'grey.700' }} /> : null;
+
+  return (
+    <MUIFormControl fullWidth>
+      <InputLabel htmlFor="outlined-adornment-email-login">{captionLabel}</InputLabel>
+      <OutlinedInput
+        id="outlined-adornment-email-login"
+        type="text"
+        value=""
+        name="email"
+        placeholder={placeholder}
+        startAdornment={
+          primaryIcon && (
+            <InputAdornment position="start">
+              {primaryIcon}
+            </InputAdornment>
+          )
+        }
+        endAdornment={
+          secondaryIcon && (
+            <InputAdornment position="end">
+              {secondaryIcon}
+            </InputAdornment>
+          )
+        }
+        label={captionLabel}
+        inputProps={{}}
+      />
+      {textPrimary && (
+        <>
+          <Divider sx={{ mt: 2 }} />
+          <div style={{ marginTop: 8 }}>
+            <span style={{ fontSize: '0.875rem', color: 'grey.700' }}>{textPrimary}</span>
+            {textSecondary && (
+              <span style={{ fontSize: '0.875rem', color: 'grey.500', marginLeft: 4 }}>{textSecondary}</span>
+            )}
+          </div>
+        </>
+      )}
+    </MUIFormControl>
+  );
+}
+
+FormControl.propTypes = {
+  captionLabel: PropTypes.string,
+  formState: PropTypes.string,
+  iconPrimary: PropTypes.any,
+  iconSecondary: PropTypes.any,
+  placeholder: PropTypes.string,
+  textPrimary: PropTypes.string,
+  textSecondary: PropTypes.string
+};
