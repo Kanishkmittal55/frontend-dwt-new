fb524ec71ac740ffd5a97f3d3f0b1d0c0063d830
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 23:23:57 +0100

    Fix Syntax Errors and Import Issues

--- src/views/dashboard/Default/BajajAreaChartCard.tsx
+++ src/views/dashboard/Default/BajajAreaChartCard.tsx
@@ -1,22 +1,52 @@
-@@ .. @@
- import { useEffect, useState } from 'react';
-+import type { FC } from 'react';
+import { useEffect, useState } from 'react';
+import type { FC } from 'react';
 
- // material-ui
- import { useTheme } from '@mui/material/styles';
-@@ .. @@
- // ===========================|| DASHBOARD DEFAULT - BAJAJ AREA CHART CARD ||=========================== //
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Card } from '@mui/material';
 
--export default function BajajAreaChartCard() {
-+const BajajAreaChartCard: FC = () => {
-   const theme = useTheme();
-   const orangeDark = theme.palette.secondary[800];
+// third-party
+import ApexCharts from 'apexcharts';
+import Chart from 'react-apexcharts';
 
-@@ .. @@
-       <Chart {...chartConfig} />
-     </Card>
-   );
--}
-+};
-+
-+export default BajajAreaChartCard;
+// project imports
+import chartData from './chart-data/bajaj-area-chart';
+
+// ===========================|| DASHBOARD DEFAULT - BAJAJ AREA CHART CARD ||=========================== //
+
+const BajajAreaChartCard: FC = () => {
+  const theme = useTheme();
+  const orangeDark = theme.palette.secondary[800];
+
+  useEffect(() => {
+    const newSupportChart = {
+      ...chartData.options,
+      colors: [orangeDark],
+      tooltip: {
+        theme: 'light'
+      }
+    };
+    ApexCharts.exec(`support-chart`, 'updateOptions', newSupportChart);
+  }, [orangeDark]);
+
+  const [series] = useState(chartData.series);
+  const [options, setOptions] = useState(chartData.options);
+
+  useEffect(() => {
+    setOptions((prevState) => ({
+      ...prevState,
+      colors: [orangeDark],
+      tooltip: {
+        theme: 'light'
+      }
+    }));
+  }, [orangeDark]);
+
+  return (
+    <Card sx={{ bgcolor: 'secondary.light' }}>
+      <Chart {...{ options, series, type: 'area', height: 95 }} />
+    </Card>
+  );
+};
+
+export default BajajAreaChartCard;
