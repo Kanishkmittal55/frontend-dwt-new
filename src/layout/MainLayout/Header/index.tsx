f5482588d639c6d25c0e4e8e4a1919c5158b12a8
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 22:57:33 +0100

    Updated index.tsx

--- src/layout/MainLayout/Header/index.tsx
+++ src/layout/MainLayout/Header/index.tsx
@@ -1,24 +1,21 @@
-@@ .. @@
- // material-ui
- import { useTheme } from '@mui/material/styles';
- import useMediaQuery from '@mui/material/useMediaQuery';
-+import type { FC } from 'react';
- import Avatar from '@mui/material/Avatar';
- import Box from '@mui/material/Box';
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import type { FC } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 
-@@ .. @@
- // ==============================|| MAIN NAVBAR / HEADER ||============================== //
+// ==============================|| MAIN NAVBAR / HEADER ||============================== //
 
--export default function Header() {
-+const Header: FC = () => {
-   const theme = useTheme();
-   const downMD = useMediaQuery(theme.breakpoints.down('md'));
+const Header: FC = () => {
+  const theme = useTheme();
+  const downMD = useMediaQuery(theme.breakpoints.down('md'));
 
-@@ .. @@
-       <ProfileSection />
-     </>
-   );
--}
-+};
-+
-+export default Header;
+  return (
+    <>
+      <ProfileSection />
+    </>
+  );
+};
+
+export default Header;
