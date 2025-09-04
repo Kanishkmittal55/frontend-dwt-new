71bdacb8af76ef5d4f8f6ed79cb9185fc909d771
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 22:43:29 +0100

    Updated FormControlSelect.tsx

--- src/ui-component/extended/Form/FormControlSelect.tsx
+++ src/ui-component/extended/Form/FormControlSelect.tsx
@@ -1,52 +1,81 @@
-@@ .. @@
--import PropTypes from 'prop-types';
- import { useState } from 'react';
-+import type { FormControlSelectProps } from 'types/utilities';
- 
- // material-ui
- import Divider from '@mui/material/Divider';
-@@ -8,7 +9,15 @@ import InputAdornment from '@mui/material/InputAdornment';
- import MenuItem from '@mui/material/MenuItem';
- import TextField from '@mui/material/TextField';
- 
--export default function FormControlSelect({
-+export default function FormControlSelect({
-   captionLabel,
-   currencies,
-   formState,
-@@ -17,7 +26,7 @@ export default function FormControlSelect({
-   selected,
-   textPrimary,
-   textSecondary
--}) {
-+}: FormControlSelectProps) {
-   const IconPrimary = iconPrimary;
-   const primaryIcon = iconPrimary ? <IconPrimary fontSize="small" sx={{ color: 'grey.700' }} /> : null;
- 
-@@ -26,8 +35,8 @@ export default function FormControlSelect({
- 
-   const errorState = formState === 'error';
-   const val = selected || '';
- 
--  const [currency, setCurrency] = useState(val);
--  const handleChange = (event) => {
-+  const [currency, setCurrency] = useState<string>(val);
-+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-     event?.target.value && setCurrency(event?.target.value);
-   };
- 
-@@ -75,15 +84,4 @@ export default function FormControlSelect({
-     </FormControl>
-   );
- }
--
--FormControlSelect.propTypes = {
--  captionLabel: PropTypes.string,
--  currencies: PropTypes.object,
--  formState: PropTypes.string,
--  iconPrimary: PropTypes.any,
--  iconSecondary: PropTypes.any,
--  selected: PropTypes.string,
--  textPrimary: PropTypes.string,
--  textSecondary: PropTypes.string
--};
+import { useState } from 'react';
+import type { FormControlSelectProps } from 'types/utilities';
+
+// material-ui
+import Divider from '@mui/material/Divider';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+
+export default function FormControlSelect({
+  captionLabel,
+  currencies,
+  formState,
+  iconPrimary,
+  iconSecondary,
+  selected,
+  textPrimary,
+  textSecondary
+}: FormControlSelectProps) {
+  const IconPrimary = iconPrimary;
+  const primaryIcon = iconPrimary ? <IconPrimary fontSize="small" sx={{ color: 'grey.700' }} /> : null;
+
+  const IconSecondary = iconSecondary;
+  const secondaryIcon = iconSecondary ? <IconSecondary fontSize="small" sx={{ color: 'grey.700' }} /> : null;
+
+  const errorState = formState === 'error';
+  const val = selected || '';
+
+  const [currency, setCurrency] = useState<string>(val);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event?.target.value && setCurrency(event?.target.value);
+  };
+
+  return (
+    <FormControl fullWidth>
+      <TextField
+        id="outlined-select-currency"
+        select
+        fullWidth
+        value={currency}
+        onChange={handleChange}
+        InputProps={{
+          startAdornment: (
+            <>
+              {primaryIcon && <InputAdornment position="start">{primaryIcon}</InputAdornment>}
+              {secondaryIcon && <InputAdornment position="start">{secondaryIcon}</InputAdornment>}
+            </>
+          )
+        }}
+        error={errorState}
+        helperText={captionLabel}
+      >
+        {currencies &&
+          Object.keys(currencies).map((option) => (
+            <MenuItem key={option} value={option}>
+              <Grid container justifyContent="space-between">
+                <Grid item>
+                  <Typography variant="subtitle1" color="inherit">
+                    {textPrimary ? textPrimary : currencies[option].label}
+                  </Typography>
+                </Grid>
+                {currencies[option].description && (
+                  <>
+                    <Grid item>
+                      <Divider />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography variant="subtitle2" color="textSecondary">
+                        {textSecondary ? textSecondary : currencies[option].description}
+                      </Typography>
+                    </Grid>
+                  </>
+                )}
+              </Grid>
+            </MenuItem>
+          ))}
+      </TextField>
+    </FormControl>
+  );
+}
