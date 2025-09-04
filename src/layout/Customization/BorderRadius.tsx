cf8971675b3ca2c2a436861f2d97e4940ac3545e
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 22:57:35 +0100

    Updated BorderRadius.tsx

--- src/layout/Customization/BorderRadius.tsx
+++ src/layout/Customization/BorderRadius.tsx
@@ -1,24 +1,42 @@
-@@ .. @@
- // material-ui
- import Grid from '@mui/material/Grid';
- import Slider from '@mui/material/Slider';
-+import type { FC } from 'react';
- import Stack from '@mui/material/Stack';
- import Typography from '@mui/material/Typography';
+// material-ui
+import Grid from '@mui/material/Grid';
+import Slider from '@mui/material/Slider';
+import type { FC } from 'react';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
-@@ .. @@
-   return `${value}px`;
- }
+function valueText(value) {
+  return `${value}px`;
+}
 
--export default function BorderRadius() {
-+const BorderRadius: FC = () => {
-   const { borderRadius, onChangeBorderRadius } = useConfig();
+const BorderRadius: FC = () => {
+  const { borderRadius, onChangeBorderRadius } = useConfig();
 
-@@ .. @@
-       </Grid>
-     </Stack>
-   );
--}
-+};
-+
-+export default BorderRadius;
+  return (
+    <Stack spacing={2.5}>
+      <Typography variant="h5">Border Radius</Typography>
+      <Grid container spacing={3} alignItems="center">
+        <Grid item>
+          <Typography variant="h6">4px</Typography>
+        </Grid>
+        <Grid item xs>
+          <Slider
+            size="small"
+            value={borderRadius}
+            onChange={onChangeBorderRadius}
+            getAriaValueText={valueText}
+            valueLabelDisplay="on"
+            valueLabelFormat={valueText}
+            min={4}
+            max={24}
+          />
+        </Grid>
+        <Grid item>
+          <Typography variant="h6">24px</Typography>
+        </Grid>
+      </Grid>
+    </Stack>
+  );
+};
+
+export default BorderRadius;
--- src/layout/MainLayout/MenuList/index.tsx
+++ src/layout/MainLayout/MenuList/index.tsx
@@ -1,22 +1,38 @@
-@@ .. @@
- import { memo, useState } from 'react';
-+import type { FC } from 'react';
- import Divider from '@mui/material/Divider';
- import List from '@mui/material/List';
- import Typography from '@mui/material/Typography';
-@@ .. @@
- // ==============================|| SIDEBAR MENU LIST ||============================== //
+import { memo, useState } from 'react';
+import type { FC } from 'react';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
 
--function MenuList() {
-+const MenuList: FC = () => {
-   const { menuMaster } = useGetMenuMaster();
-   const drawerOpen = menuMaster.isDashboardDrawerOpened;
+// ==============================|| SIDEBAR MENU LIST ||============================== //
 
-@@ .. @@
-   });
+const MenuList: FC = () => {
+  const { menuMaster } = useGetMenuMaster();
+  const drawerOpen = menuMaster.isDashboardDrawerOpened;
 
-   return <Box {...(drawerOpen && { sx: { mt: 1.5 } })}>{navItems}</Box>;
--}
-+};
+  const navItems = menuItems.map((item) => {
+    switch (item.type) {
+      case 'collapse':
+        return (
+          <NavCollapse
+            key={item.id}
+            menu={item}
+            level={1}
+            parentId={item.id}
+          />
+        );
+      case 'item':
+        return <NavItem key={item.id} item={item} level={1} />;
+      default:
+        return (
+          <Typography key={item.id} variant="h6" color="error" align="center">
+            Fix - Menu Items
+          </Typography>
+        );
+    }
+  });
 
- export default memo(MenuList);
+  return <Box {...(drawerOpen && { sx: { mt: 1.5 } })}>{navItems}</Box>;
+};
+
+export default memo(MenuList);
