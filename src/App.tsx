f89ae1ffb50274c25031ff2850dae496d4765ddb
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 22:29:51 +0100

    Phase 1.2: File Extension Migration

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+@@ .. @@
+ import { RouterProvider } from 'react-router-dom';
+ 
+ // routing
+-import router from 'routes';
++import router from 'routes/index';
+ 
+ // project imports
+ import NavigationScroll from 'layout/NavigationScroll';
+ 
+ import ThemeCustomization from 'themes';
+ 
+ // auth provider
+ 
+ // ==============================|| APP ||============================== //
+ 
+ export default function App() {
+   return (
+     <ThemeCustomization>
+       <NavigationScroll>
+         <>
+           <RouterProvider router={router} />
+         </>
+       </NavigationScroll>
+     </ThemeCustomization>
+   );
+ }
---
+++ src/config.ts
@@ -0,0 +1,9 @@
+@@ .. @@
+ export const DASHBOARD_PATH = '/sample-page';
+ 
+ const config = {
+   fontFamily: `'Roboto', sans-serif`,
+   borderRadius: 8
+ };
+ 
+ export default config;
