d8a4123ed18675f8126eaf5467f942a781f07af4
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2025-09-04 22:57:46 +0100

    Updated index.tsx

--- src/layout/MainLayout/MenuList/NavItem/index.tsx
+++ src/layout/MainLayout/MenuList/NavItem/index.tsx
@@ -1,34 +1,138 @@
-@@ .. @@
- import PropTypes from 'prop-types';
- import { useEffect, useRef, useState } from 'react';
- import { Link, matchPath, useLocation } from 'react-router-dom';
-+import type { FC } from 'react';
-+import type { MenuItem } from 'types';
+import PropTypes from 'prop-types';
+import { useEffect, useRef, useState } from 'react';
+import { Link, matchPath, useLocation } from 'react-router-dom';
+import type { FC } from 'react';
+import type { MenuItem } from 'types';
 
- // material-ui
- import { useTheme } from '@mui/material/styles';
-@@ .. @@
- // assets
- import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Avatar, Chip, ListItemButton, ListItemIcon, ListItemText, Typography, useMediaQuery } from '@mui/material';
 
--export default function NavItem({ item, level, isParents = false, setSelectedID }) {
-+interface NavItemProps {
-+  item: MenuItem;
-+  level: number;
-+  isParents?: boolean;
-+  setSelectedID?: () => void;
-+}
-+
-+const NavItem: FC<NavItemProps> = ({ item, level, isParents = false, setSelectedID }) => {
-   const theme = useTheme();
-   const downMD = useMediaQuery(theme.breakpoints.down('md'));
-   const ref = useRef(null);
-@@ .. @@
-       </ListItemButton>
-     </>
-   );
--}
-+};
+// project import
+import { handlerActiveItem, useGetMenuMaster } from 'api/menu';
 
--NavItem.propTypes = { item: PropTypes.any, level: PropTypes.number, isParents: PropTypes.bool, setSelectedID: PropTypes.func };
-+export default NavItem;
+// assets
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+interface NavItemProps {
+  item: MenuItem;
+  level: number;
+  isParents?: boolean;
+  setSelectedID?: () => void;
+}
+
+const NavItem: FC<NavItemProps> = ({ item, level, isParents = false, setSelectedID }) => {
+  const theme = useTheme();
+  const downMD = useMediaQuery(theme.breakpoints.down('md'));
+  const ref = useRef(null);
+  const { pathname } = useLocation();
+  const { menuMaster } = useGetMenuMaster();
+  const drawerOpen = menuMaster.isDashboardDrawerOpened;
+
+  const [selected, setSelected] = useState(false);
+
+  useEffect(() => {
+    if (matchPath({ path: item.url, exact: false }, pathname)) {
+      if (setSelectedID) {
+        setSelectedID();
+      }
+      setSelected(true);
+    } else {
+      setSelected(false);
+    }
+  }, [pathname, item.url, setSelectedID]);
+
+  const textColor = 'text.primary';
+  const iconSelectedColor = 'primary.main';
+
+  return (
+    <>
+      <ListItemButton
+        ref={ref}
+        component={Link}
+        to={item.url}
+        disabled={item.disabled}
+        onClick={() => handlerActiveItem(item.id)}
+        selected={selected}
+        sx={{
+          zIndex: 1201,
+          pl: drawerOpen ? `${level * 28}px` : 1.5,
+          py: !drawerOpen && level === 1 ? 1.25 : 1,
+          ...(drawerOpen && {
+            '&:hover': {
+              bgcolor: 'primary.lighter'
+            },
+            '&.Mui-selected': {
+              bgcolor: 'primary.lighter',
+              borderRight: `2px solid ${theme.palette.primary.main}`,
+              color: iconSelectedColor,
+              '&:hover': {
+                color: iconSelectedColor,
+                bgcolor: 'primary.lighter'
+              }
+            }
+          }),
+          ...(!drawerOpen && {
+            '&:hover': {
+              bgcolor: 'transparent'
+            },
+            '&.Mui-selected': {
+              '&:hover': {
+                bgcolor: 'transparent'
+              },
+              bgcolor: 'transparent'
+            }
+          })
+        }}
+      >
+        {item.icon && (
+          <ListItemIcon
+            sx={{
+              minWidth: 28,
+              color: selected ? iconSelectedColor : textColor,
+              ...(!drawerOpen && {
+                borderRadius: 1.5,
+                width: 36,
+                height: 36,
+                alignItems: 'center',
+                justifyContent: 'center',
+                '&:hover': {
+                  bgcolor: 'secondary.lighter'
+                }
+              }),
+              ...(!drawerOpen &&
+                selected && {
+                  bgcolor: 'primary.lighter',
+                  '&:hover': {
+                    bgcolor: 'primary.lighter'
+                  }
+                })
+            }}
+          >
+            {item.icon}
+          </ListItemIcon>
+        )}
+        {(drawerOpen || (!drawerOpen && level !== 1)) && (
+          <ListItemText
+            primary={
+              <Typography variant="h6" sx={{ color: selected ? iconSelectedColor : textColor }}>
+                {item.title}
+              </Typography>
+            }
+          />
+        )}
+        {(drawerOpen || (!drawerOpen && level !== 1)) && item.chip && (
+          <Chip
+            color={item.chip.color}
+            variant={item.chip.variant}
+            size={item.chip.size}
+            label={item.chip.label}
+            avatar={item.chip.avatar && <Avatar>{item.chip.avatar}</Avatar>}
+          />
+        )}
+      </ListItemButton>
+    </>
+  );
+};
+
+export default NavItem;
